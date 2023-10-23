8045dcacc2180c3b14ab9643604c6e1f29d20fe6
Samba@X13 <santos.diogo.21@outlook.com>
2023-10-23 09:36:01 +0100

    Redefinição dos tipos de mensagem nos pacotes de intereação cliente-servidor

--- src/FSProtocol/FSTrackerProtocol.java
+++ src/FSProtocol/FSTrackerProtocol.java
@@ -4,9 +4,18 @@ import java.io.*;
 
 public class FSTrackerProtocol implements Serializable 
 {
+    public enum TypeMsg
+    {
+        REG,
+        ACK,
+        AVF,
+        GET
+    };
+
+
     //Packages are immutable
     private final InetAddress srcIP; // Endereço IP de origem
-    private final short type; // Tipo da mensagem
+    private final TypeMsg type ;
     private final byte[] payload;
 
     @Override
@@ -15,25 +24,10 @@ public class FSTrackerProtocol implements Serializable
         return "srcIP="+ srcIP+ " type="+ type; 
     }
 
-    private short type_translate (String str)
-    {
-        switch (str)
-        {
-            case "END":
-                return 1;
-            case "REG":
-                return 2;
-            //mais mensagens
-            
-            default:
-                return 0;
-        }
-    }
-
-    public FSTrackerProtocol(byte[] payload, String type, InetAddress srcIP)
+    public FSTrackerProtocol(byte[] payload, TypeMsg type, InetAddress srcIP)
     {
         this.payload = payload;
-        this.type = type_translate(type);
+        this.type = type;
         this.srcIP = srcIP;
     }
 
@@ -42,8 +36,14 @@ public class FSTrackerProtocol implements Serializable
         return srcIP;
     }
 
+    public TypeMsg getTypeMsg()
+    {
+        return type;
+    }
+
     public byte[] getPayload()
     {
         return payload;
     }
+
 }
