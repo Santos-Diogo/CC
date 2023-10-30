4751154ea31ad53593b97664a5264cf62e26eb40
Martim Félix <martimfelix@Mac-Martim.local>
2023-10-30 10:30:07 +0000

    Passo 1 e 2 feito

--- src/FSProtocol/FSTrackerProtocol.java
+++ src/FSProtocol/FSTrackerProtocol.java
@@ -1,9 +1,30 @@
 package FSProtocol;
+import FSNode.FSNode;
+
 import java.net.*;
 import java.io.*;
 
 public class FSTrackerProtocol implements Serializable 
 {
+    private String nomeDoArquivo;
+    private FSNode fsNode;
+
+    public void setNomeDoArquivo(String nomeDoArquivo) {
+        this.nomeDoArquivo = nomeDoArquivo;
+    }
+
+    public String getNomeArquivo(){
+        return nomeDoArquivo;
+    }
+
+    public void setFSNode(FSNode fsNode) {
+        this.fsNode = fsNode;
+    }
+
+    public FSNode getFSNode(){
+        return fsNode;
+    }
+
     public enum TypeMsg
     {
         REG,
--- src/FSTracker/TrackerStorage.java
+++ src/FSTracker/TrackerStorage.java
@@ -7,10 +7,12 @@ import FSNode.FSNode;
 import FSProtocol.FSTrackerProtocol;
 
 public class TrackerStorage {
-    private Map<String,List<FSNode>> fileToNodesMap = new HashMap<>();
-    private Map <FSNode,List<String>> nodeToFilesMap = new HashMap<>();
+    private final Map<String,List<FSNode>> fileToNodesMap = new HashMap<>();
+    private final Map <FSNode,List<String>> nodeToFilesMap = new HashMap<>();
     private static PacketManager pm;
     private String nomeDoArquivo;
+    private FSNode fsNode;
+
 
     public void adicionarArquivoAoFSNode (String nomeArquivo,FSNode fsNode){
         if(fileToNodesMap.containsKey(nomeArquivo)){
@@ -24,23 +26,15 @@ public class TrackerStorage {
         }
     }
 
-    public void setNomeDoArquivo(String nomeDoArquivo) {
-        this.nomeDoArquivo = nomeDoArquivo;
-    }
-
-    public String getNomeArquivo(){
-        return nomeDoArquivo
-    }
-
-
 
     public static void main(String[] args){
+        TrackerStorage trackerStorage = new TrackerStorage();
         FSTrackerProtocol receivedPacket = pm.get_packet();
         FSTrackerProtocol.TypeMsg packetType = receivedPacket.getTypeMsg();
         if (packetType == FSTrackerProtocol.TypeMsg.REG) {
-            String nomeArquivo = receivedPacket.getNomeArquivo();              //getNomeArquivo ainda tem de ser implementada
-            FSNode fsNode = receivedPacket.getFSNode();                        //getFSNode ainda tem de ser implementada    
-            adicionarArquivoAoFSNode(nomeArquivo,FSNode);                      // adicionarArquivoAoFSNode ainda tem de ser implementado e serve para atualizar as estruturas de dados!
+            String nomeArquivo = receivedPacket.getNomeArquivo();
+            FSNode fsNode = receivedPacket.getFSNode();
+            trackerStorage.adicionarArquivoAoFSNode(nomeArquivo,fsNode);                      // adicionarArquivoAoFSNode ainda tem de ser implementado e serve para atualizar as estruturas de dados!
         }
     }
 
