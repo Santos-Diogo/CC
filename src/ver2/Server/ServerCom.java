9daea4177520ef86caefb291ba1ffb2430dc4aaf
luis ribeiro <a100608@alunos.uminho.pt>
2023-11-03 15:32:11 +0000

    Wrepetuu

--- src/ver2/Server/ServerCom.java
+++ src/ver2/Server/ServerCom.java
@@ -9,19 +9,17 @@ import ver2.ThreadTools.ThreadControl;
 import ver2.Track_Protocol.Track_Packet;
 
 /**
- * Class responsible for handling communication for each independent node on the server side.
+ * Class responsible for handling communication for each independent node on the
+ * server side.
  */
-public class ServerCom implements Runnable
-{
+public class ServerCom implements Runnable {
     private static ThreadControl tc;
     private static Socket socket;
     private static ObjectInputStream input;
     private static SharedState ss;
 
-    private void handle (Track_Packet packet)
-    {
-        switch (packet.getType())
-        {
+    private void handle(Track_Packet packet) {
+        switch (packet.getType()) {
             case REG:
                 System.out.println("REG message");
                 break;
@@ -35,35 +33,30 @@ public class ServerCom implements Runnable
                 System.out.println("GET message");
                 break;
             default:
-                
+
         }
-        
+
     }
 
-    public ServerCom (Socket socket, ThreadControl tc, SharedState ss) throws IOException
-    {
-        this.socket= socket;
-        this.tc= tc;
-        this.ss= ss;
+    public ServerCom(Socket socket, ThreadControl tc, SharedState ss) throws IOException {
+        this.socket = socket;
+        this.tc = tc;
+        this.ss = ss;
         this.input = new ObjectInputStream(socket.getInputStream());
     }
 
-    public void run ()
-    {
+    public void run() {
         Track_Packet packet;
-        System.out.println("WREPETUUUUU");
-        while (tc.end_check()!= false)
-        {
-            try
-            {
-                packet= (Track_Packet) input.readObject();
-                handle (packet);
-            }
-            catch (IOException | ClassNotFoundException e)
-            {
+
+        while (tc.end_check() != false) {
+            try {
+                System.out.println("WREPETUUUUU");
+                packet = (Track_Packet) input.readObject();
+                handle(packet);
+            } catch (IOException | ClassNotFoundException e) {
                 System.out.println("ServerCom Thread encountered a problem while reading from client");
                 e.printStackTrace();
             }
         }
-    }       
+    }
 }
