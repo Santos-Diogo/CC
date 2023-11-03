abd2324c407b1720f90143b842eb72495489b86f
Samba@WS <santos.diogo.21@outlook.com>
2023-11-03 15:56:50 +0000

    Mudança na maneira de ler packets

--- src/ver2/Server/ServerCom.java
+++ src/ver2/Server/ServerCom.java
@@ -12,14 +12,17 @@ import ver2.Track_Protocol.Track_Packet;
  * Class responsible for handling communication for each independent node on the
  * server side.
  */
-public class ServerCom implements Runnable {
+public class ServerCom implements Runnable 
+{
     private ThreadControl tc;
     private Socket socket;
     private ObjectInputStream input;
     private SharedState ss;
 
-    private void handle(Track_Packet packet) {
-        switch (packet.getType()) {
+    private void handle(Track_Packet packet) 
+    {
+        switch (packet.getType()) 
+        {
             case REG:
                 System.out.println("REG message");
                 break;
@@ -38,26 +41,29 @@ public class ServerCom implements Runnable {
 
     }
 
-    public ServerCom(Socket socket, ThreadControl tc, SharedState ss) throws IOException {
+    public ServerCom(Socket socket, ThreadControl tc, SharedState ss) throws IOException 
+    {
         this.socket = socket;
         this.tc = tc;
         this.ss = ss;
         this.input = new ObjectInputStream(socket.getInputStream());
     }
 
-    public void run() {
+    public void run() 
+    {
         Track_Packet packet;
 
-        while (tc.get_running() == true && !this.socket.isClosed()) {
-            try {
-
+        while (tc.get_running() == true && !this.socket.isClosed()) 
+        {
+            try 
+            {
                 packet = (Track_Packet) input.readObject();
-                handle(packet);
-            } catch (IOException | ClassNotFoundException e) {
-                System.out.println("ServerCom Thread encountered a problem while reading from client");
-                e.printStackTrace();
+            }
+            catch (IOException | ClassNotFoundException e) 
+            {
                 break;
             }
+            handle(packet);
         }
     }
 }
