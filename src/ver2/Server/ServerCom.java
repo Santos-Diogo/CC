03840899e04203937229d870a6631ade2cb61ee2
luis ribeiro <a100608@alunos.uminho.pt>
2023-11-05 23:17:56 +0000

    Adicionar um menuzito ao node

--- src/ver2/Server/ServerCom.java
+++ src/ver2/Server/ServerCom.java
@@ -12,25 +12,19 @@ import ver2.Track_Protocol.Track_Packet;
  * Class responsible for handling communication for each independent node on the
  * server side.
  */
-public class ServerCom implements Runnable 
-{
+public class ServerCom implements Runnable {
     private ThreadControl tc;
     private Socket socket;
     private ObjectInputStream input;
     private SharedState ss;
 
-    private void handle(Track_Packet packet) 
-    {
-        switch (packet.getType()) 
-        {
+    private void handle(Track_Packet packet) {
+        switch (packet.getType()) {
             case REG:
                 System.out.println("REG message");
                 break;
-            case ACK:
-                System.out.println("ACK message");
-                break;
-            case AVF:
-                System.out.println("AVF message");
+            case AVF_REQ:
+                System.out.println("AVF request message");
                 break;
             case GET:
                 System.out.println("GET message");
@@ -41,26 +35,20 @@ public class ServerCom implements Runnable
 
     }
 
-    public ServerCom(Socket socket, ThreadControl tc, SharedState ss) throws IOException 
-    {
+    public ServerCom(Socket socket, ThreadControl tc, SharedState ss) throws IOException {
         this.socket = socket;
         this.tc = tc;
         this.ss = ss;
         this.input = new ObjectInputStream(socket.getInputStream());
     }
 
-    public void run() 
-    {
+    public void run() {
         Track_Packet packet;
 
-        while (tc.get_running()== true) 
-        {
-            try 
-            {
+        while (tc.get_running() == true) {
+            try {
                 packet = (Track_Packet) input.readObject();
-            }
-            catch (IOException | ClassNotFoundException e) 
-            {
+            } catch (IOException | ClassNotFoundException e) {
                 break;
             }
             handle(packet);
