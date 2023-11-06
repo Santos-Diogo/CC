54c1e5e6b84f32cf365169899f1ac4926c5dcf9b
Samba@WS <santos.diogo.21@outlook.com>
2023-11-06 17:23:44 +0000

    Definição do ServerInfo. Construtores das classes definidas. Alguns métodos.

--- src/FileInfo/NodeInfo.java
+++ src/FileInfo/NodeInfo.java
@@ -1,8 +1,7 @@
 package FileInfo;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 public class NodeInfo
 {
@@ -10,13 +9,64 @@ public class NodeInfo
     private Map<String, Integer> files_size;
     private Map<String, List<Integer>> files_blocks;
 
+    /**
+     * @param dir Direcotry from wich we read the files
+     * @apiNote If the file as all the blocks leave the List in files_blocks as 0(null) =)
+     */
     public NodeInfo (String dir)
     {
-
+        //handle meta-data file
     }
 
-    public add_file (String file, int size, int blocks)
+    /**
+     * Add a file to the Structure
+     * 
+     * @param name  name of the file
+     * @param size  size of the file in blocks
+     * @param blocks    blocks owned. (null for all blocks)
+     */
+    public void add_file (String name, int size, List<Integer> blocks)
     {
+        files.add (name);
+        files_size.put (name, size);
+        files_blocks.put (name, blocks);
+    }
 
+    /**
+     * Remove a file with a given name from the node info
+     * 
+     * @param name name of the file
+     */
+    public void rm_file (String name)
+    {
+        files.removeIf(file -> file.equals(name));
+        files_size.remove(name);
+        files_blocks.remove(name);
+    }
+
+    /**
+     * @return returns the names of the files stored
+     */
+    public List<String> get_files ()
+    {
+        return files;
+    }
+
+    /**
+     * @param file name of the file
+     * @return returns the number of blocks of a given file
+     */
+    public int get_file_size (String file)
+    {
+        return files_size.get(file);
+    }
+
+    /**
+     * @param file name of the file
+     * @return returns the blocks the node has of a given file
+     */
+    public List<Integer> get_file_blocks (String file)
+    {
+        return files_blocks.get(file);
     }
 }
