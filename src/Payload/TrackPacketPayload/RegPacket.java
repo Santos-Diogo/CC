9b0923b64bebd787bb7694cfd6425ec18086e4e8
luis ribeiro <a100608@alunos.uminho.pt>
2023-11-18 18:18:43 +0000

    Isto também

--- src/Payload/TrackPacketPayload/RegPacket.java
+++ src/Payload/TrackPacketPayload/RegPacket.java
@@ -1,26 +1,23 @@
 package Payload.TrackPacketPayload;
 
+import java.io.Serializable;
 import java.util.*;
 
 /**
  * Payload for the Reg Messages
  */
-public class RegPacket implements TrackPacketPayload
-{
+public class RegPacket implements TrackPacketPayload, Serializable {
     private Map<String, List<Integer>> files_blocks;
 
-    public RegPacket (Map<String, List<Integer>> files_blocks)
-    {
-        files_blocks= new HashMap<>(files_blocks);
+    public RegPacket(Map<String, List<Integer>> files_blocks) {
+        files_blocks = new HashMap<>(files_blocks);
     }
 
-    public Map<String, List<Integer>> get_files_blocks() 
-    {
+    public Map<String, List<Integer>> get_files_blocks() {
         Map<String, List<Integer>> clonedMap = new HashMap<>();
 
-        for (Map.Entry<String, List<Integer>> entry : this.files_blocks.entrySet()) 
-        {
-            //Clone the List
+        for (Map.Entry<String, List<Integer>> entry : this.files_blocks.entrySet()) {
+            // Clone the List
             List<Integer> clonedList = new ArrayList<>(entry.getValue());
 
             clonedMap.put(entry.getKey(), clonedList);
@@ -29,10 +26,9 @@ public class RegPacket implements TrackPacketPayload
         return clonedMap;
     }
 
-    public void add_file_blocks (String file, List<Integer> blocks)
-    {
-        //clone blocks
-        List<Integer> l= new ArrayList<>(blocks);
+    public void add_file_blocks(String file, List<Integer> blocks) {
+        // clone blocks
+        List<Integer> l = new ArrayList<>(blocks);
         files_blocks.put(file, l);
     }
 }
