daeb47533b80b29c47bb54e15728ce7b49482268
luis ribeiro <a100608@alunos.uminho.pt>
2023-11-20 09:26:50 +0000

    Adição de msg de disconnect

--- src/Track_Protocol/TrackPacket.java
+++ src/Track_Protocol/TrackPacket.java
@@ -4,45 +4,40 @@ import java.io.Serializable;
 import java.net.InetAddress;
 import Payload.TrackPacketPayload.*;
 
-public class TrackPacket implements Serializable 
-{
-    public enum TypeMsg 
-    {
-        REG,        //Register
-        AVF_REQ,    //Available Files Request
-        AVF_RESP,   //Available Files Response
-        ADD_F,      //Add a File/ Blocks
-        RM_F,       //Remove a File/ Blocks
-        GET_REQ,    //Get files request
-        GET_RESP    //Get files response
+public class TrackPacket implements Serializable {
+    public enum TypeMsg {
+        REG, // Register
+        AVF_REQ, // Available Files Request
+        AVF_RESP, // Available Files Response
+        ADD_F, // Add a File/ Blocks
+        RM_F, // Remove a File/ Blocks
+        DC, // Node disconnected from network
+        GET_REQ, // Get files request
+        GET_RESP // Get files response
     }
 
     private InetAddress src_ip;
     private TypeMsg type;
     private TrackPacketPayload payload;
 
-    public TrackPacket(InetAddress src_ip, TypeMsg type, TrackPacketPayload payload) 
-    {
-        this.src_ip= src_ip;
-        this.type= type;
-        this.payload= payload;
+    public TrackPacket(InetAddress src_ip, TypeMsg type, TrackPacketPayload payload) {
+        this.src_ip = src_ip;
+        this.type = type;
+        this.payload = payload;
     }
 
-    public InetAddress getSrc_ip() 
-    {
+    public InetAddress getSrc_ip() {
         return src_ip;
     }
 
-    public TypeMsg getType ()
-    {
+    public TypeMsg getType() {
         return type;
     }
 
     /**
      * @return Returns the payload directly (No copy made)
      */
-    public TrackPacketPayload getPayload()
-    {
+    public TrackPacketPayload getPayload() {
         return payload;
     }
 }
