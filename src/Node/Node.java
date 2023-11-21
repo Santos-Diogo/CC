31d4a6aa4b12bc6820cf0f94f9f366fbc3159244
Samba@WS <santos.diogo.21@outlook.com>
2023-11-21 17:34:44 +0000

    Transfer Ports

--- src/Node/Node.java
+++ src/Node/Node.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.*;
--- src/Shared/Defines.java
+++ src/Shared/Defines.java
@@ -2,5 +2,6 @@ package Shared;
 
 public class Defines 
 {
-    public static final int nodeHostPort= 7575;
+    public static final int trackerPort= 9090;
+    public static final int transferPort= 9090;
 }
