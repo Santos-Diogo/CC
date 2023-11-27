d707a949c498fa487943c4dcf40c29a0a2b20b46
Martim Félix <a100647@alunos.uminho.pt>
2023-11-27 09:10:58 +0000

    Delete

--- src/TransferProtocol/AckPacket.java
+++
@@ -1,8 +0,0 @@
-package Payload.TransferPacketPayload;
-import java.io.Serializable;
-import java.util.*;
-
-
-// Payload para mensagem 'ACK'
-public class AckPacket implements TransferPacketPayload, Serializable {
-}
--- src/TransferProtocol/ErrPacket.java
+++
@@ -1,8 +0,0 @@
-package Payload.TransferPacketPayload;
-import java.io.Serializable;
-import java.util.*;
-
-
-// Payload para mensagem 'Error'
-public class ErrPacket implements TransferPacketPayload, Serializable {
-}
